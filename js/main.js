02b192e54cb91b69a17e5414a55f7503468f4b48
RongGitpro <2313104423@qq.com>
2024-10-04 15:34:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,150 +2,150 @@ var board = new Array();
 var hasConflicted = new Array();
 var score = 0;
 
-// $(function () {
-//     newgame();
-// });
-
-let touchStartX = 0;
-let touchStartY = 0;
-
 $(function () {
     newgame();
-
-    // 添加触摸事件
-    $('#grid-container').on('touchstart', function(event) {
-        touchStartX = event.touches[0].clientX;
-        touchStartY = event.touches[0].clientY;
-    });
-
-    $('#grid-container').on('touchmove', function(event) {
-        event.preventDefault(); // 防止页面滚动
-    });
-
-    $('#grid-container').on('touchend', function(event) {
-        const touchEndX = event.changedTouches[0].clientX;
-        const touchEndY = event.changedTouches[0].clientY;
-
-        const deltaX = touchEndX - touchStartX;
-        const deltaY = touchEndY - touchStartY;
-
-        if (Math.abs(deltaX) > Math.abs(deltaY)) {
-            // 水平移动
-            if (deltaX > 0) {
-                moveRight(); // 向右移动
-            } else {
-                moveLeft(); // 向左移动
-            }
-        } else {
-            // 垂直移动
-            if (deltaY > 0) {
-                moveDown(); // 向下移动
-            } else {
-                moveUp(); // 向上移动
-            }
-        }
-    });
 });
 
-//左边的移动函数
-function moveLeft() {
-    if (!canMove(board)) return;
+// let touchStartX = 0;
+// let touchStartY = 0;
 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 1; j < 4; j++) {
-            if (board[i][j] !== 0) {
-                let targetIndex = j;
-                while (targetIndex > 0 && board[i][targetIndex - 1] === 0) {
-                    board[i][targetIndex - 1] = board[i][targetIndex];
-                    board[i][targetIndex] = 0;
-                    targetIndex--;
-                }
-                if (targetIndex > 0 && board[i][targetIndex - 1] === board[i][targetIndex] && !hasConflicted[i][targetIndex - 1]) {
-                    board[i][targetIndex - 1] += board[i][targetIndex];
-                    board[i][targetIndex] = 0;
-                    hasConflicted[i][targetIndex - 1] = true; // 标记合并过的方块
-                }
-            }
-        }
-    }
-    generateOneNumber();
-    updateBoardView();
-}
+// $(function () {
+//     newgame();
 
-//右边的移动函数
-function moveRight() {
-    if (!canMove(board)) return;
+//     // 添加触摸事件
+//     $('#grid-container').on('touchstart', function(event) {
+//         touchStartX = event.touches[0].clientX;
+//         touchStartY = event.touches[0].clientY;
+//     });
 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 2; j >= 0; j--) {
-            if (board[i][j] !== 0) {
-                let targetIndex = j;
-                while (targetIndex < 3 && board[i][targetIndex + 1] === 0) {
-                    board[i][targetIndex + 1] = board[i][targetIndex];
-                    board[i][targetIndex] = 0;
-                    targetIndex++;
-                }
-                if (targetIndex < 3 && board[i][targetIndex + 1] === board[i][targetIndex] && !hasConflicted[i][targetIndex + 1]) {
-                    board[i][targetIndex + 1] += board[i][targetIndex];
-                    board[i][targetIndex] = 0;
-                    hasConflicted[i][targetIndex + 1] = true; // 标记合并过的方块
-                }
-            }
-        }
-    }
-    generateOneNumber();
-    updateBoardView();
-}
+//     $('#grid-container').on('touchmove', function(event) {
+//         event.preventDefault(); // 防止页面滚动
+//     });
 
-//向上的移动函数
-function moveUp() {
-    if (!canMove(board)) return;
+//     $('#grid-container').on('touchend', function(event) {
+//         const touchEndX = event.changedTouches[0].clientX;
+//         const touchEndY = event.changedTouches[0].clientY;
 
-    for (let j = 0; j < 4; j++) {
-        for (let i = 1; i < 4; i++) {
-            if (board[i][j] !== 0) {
-                let targetIndex = i;
-                while (targetIndex > 0 && board[targetIndex - 1][j] === 0) {
-                    board[targetIndex - 1][j] = board[targetIndex][j];
-                    board[targetIndex][j] = 0;
-                    targetIndex--;
-                }
-                if (targetIndex > 0 && board[targetIndex - 1][j] === board[targetIndex][j] && !hasConflicted[targetIndex - 1][j]) {
-                    board[targetIndex - 1][j] += board[targetIndex][j];
-                    board[targetIndex][j] = 0;
-                    hasConflicted[targetIndex - 1][j] = true; // 标记合并过的方块
-                }
-            }
-        }
-    }
-    generateOneNumber();
-    updateBoardView();
-}
+//         const deltaX = touchEndX - touchStartX;
+//         const deltaY = touchEndY - touchStartY;
 
-//向下的移动函数
-function moveDown() {
-    if (!canMove(board)) return;
+//         if (Math.abs(deltaX) > Math.abs(deltaY)) {
+//             // 水平移动
+//             if (deltaX > 0) {
+//                 moveRight(); // 向右移动
+//             } else {
+//                 moveLeft(); // 向左移动
+//             }
+//         } else {
+//             // 垂直移动
+//             if (deltaY > 0) {
+//                 moveDown(); // 向下移动
+//             } else {
+//                 moveUp(); // 向上移动
+//             }
+//         }
+//     });
+// });
 
-    for (let j = 0; j < 4; j++) {
-        for (let i = 2; i >= 0; i--) {
-            if (board[i][j] !== 0) {
-                let targetIndex = i;
-                while (targetIndex < 3 && board[targetIndex + 1][j] === 0) {
-                    board[targetIndex + 1][j] = board[targetIndex][j];
-                    board[targetIndex][j] = 0;
-                    targetIndex++;
-                }
-                if (targetIndex < 3 && board[targetIndex + 1][j] === board[targetIndex][j] && !hasConflicted[targetIndex + 1][j]) {
-                    board[targetIndex + 1][j] += board[targetIndex][j];
-                    board[targetIndex][j] = 0;
-                    hasConflicted[targetIndex + 1][j] = true; // 标记合并过的方块
-                }
-            }
-        }
-    }
-    generateOneNumber();
-    updateBoardView();
-}
+// //左边的移动函数
+// function moveLeft() {
+//     if (!canMove(board)) return;
+
+//     for (let i = 0; i < 4; i++) {
+//         for (let j = 1; j < 4; j++) {
+//             if (board[i][j] !== 0) {
+//                 let targetIndex = j;
+//                 while (targetIndex > 0 && board[i][targetIndex - 1] === 0) {
+//                     board[i][targetIndex - 1] = board[i][targetIndex];
+//                     board[i][targetIndex] = 0;
+//                     targetIndex--;
+//                 }
+//                 if (targetIndex > 0 && board[i][targetIndex - 1] === board[i][targetIndex] && !hasConflicted[i][targetIndex - 1]) {
+//                     board[i][targetIndex - 1] += board[i][targetIndex];
+//                     board[i][targetIndex] = 0;
+//                     hasConflicted[i][targetIndex - 1] = true; // 标记合并过的方块
+//                 }
+//             }
+//         }
+//     }
+//     generateOneNumber();
+//     updateBoardView();
+// }
+
+// //右边的移动函数
+// function moveRight() {
+//     if (!canMove(board)) return;
+
+//     for (let i = 0; i < 4; i++) {
+//         for (let j = 2; j >= 0; j--) {
+//             if (board[i][j] !== 0) {
+//                 let targetIndex = j;
+//                 while (targetIndex < 3 && board[i][targetIndex + 1] === 0) {
+//                     board[i][targetIndex + 1] = board[i][targetIndex];
+//                     board[i][targetIndex] = 0;
+//                     targetIndex++;
+//                 }
+//                 if (targetIndex < 3 && board[i][targetIndex + 1] === board[i][targetIndex] && !hasConflicted[i][targetIndex + 1]) {
+//                     board[i][targetIndex + 1] += board[i][targetIndex];
+//                     board[i][targetIndex] = 0;
+//                     hasConflicted[i][targetIndex + 1] = true; // 标记合并过的方块
+//                 }
+//             }
+//         }
+//     }
+//     generateOneNumber();
+//     updateBoardView();
+// }
+
+// //向上的移动函数
+// function moveUp() {
+//     if (!canMove(board)) return;
+
+//     for (let j = 0; j < 4; j++) {
+//         for (let i = 1; i < 4; i++) {
+//             if (board[i][j] !== 0) {
+//                 let targetIndex = i;
+//                 while (targetIndex > 0 && board[targetIndex - 1][j] === 0) {
+//                     board[targetIndex - 1][j] = board[targetIndex][j];
+//                     board[targetIndex][j] = 0;
+//                     targetIndex--;
+//                 }
+//                 if (targetIndex > 0 && board[targetIndex - 1][j] === board[targetIndex][j] && !hasConflicted[targetIndex - 1][j]) {
+//                     board[targetIndex - 1][j] += board[targetIndex][j];
+//                     board[targetIndex][j] = 0;
+//                     hasConflicted[targetIndex - 1][j] = true; // 标记合并过的方块
+//                 }
+//             }
+//         }
+//     }
+//     generateOneNumber();
+//     updateBoardView();
+// }
+
+// //向下的移动函数
+// function moveDown() {
+//     if (!canMove(board)) return;
+
+//     for (let j = 0; j < 4; j++) {
+//         for (let i = 2; i >= 0; i--) {
+//             if (board[i][j] !== 0) {
+//                 let targetIndex = i;
+//                 while (targetIndex < 3 && board[targetIndex + 1][j] === 0) {
+//                     board[targetIndex + 1][j] = board[targetIndex][j];
+//                     board[targetIndex][j] = 0;
+//                     targetIndex++;
+//                 }
+//                 if (targetIndex < 3 && board[targetIndex + 1][j] === board[targetIndex][j] && !hasConflicted[targetIndex + 1][j]) {
+//                     board[targetIndex + 1][j] += board[targetIndex][j];
+//                     board[targetIndex][j] = 0;
+//                     hasConflicted[targetIndex + 1][j] = true; // 标记合并过的方块
+//                 }
+//             }
+//         }
+//     }
+//     generateOneNumber();
+//     updateBoardView();
+// }
 
 
 
